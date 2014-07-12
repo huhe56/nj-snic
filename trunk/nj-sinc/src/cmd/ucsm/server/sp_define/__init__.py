000168d7db05312532f5140e64fcd58002ea0fc7
huan.he@gmail.com <huan.he@gmail.com@c3881051-5558-5a05-daaa-2409dd267fac>
2014-07-12 20:06:34 +0000

    combine into all

--- trunk/nj-sinc/src/cmd/ucsm/server/sp_define/__init__.py
+++ trunk/nj-sinc/src/cmd/ucsm/server/sp_define/__init__.py
@@ -1,9 +1,44 @@
 
+import pprint
+
+from main.define import Define
+from lib.util import Util
+
+
+raid_level_disk_group_config_policy_dict = {
+                   0: {'policy_name': 'raid0striped',  'policy_raid_level_name': 'raid-0-striped'},
+                   1: {'policy_name': 'raid1mirrored', 'policy_raid_level_name': 'raid-1-mirrored'},
+                   }
+config = {
+          1: {
+              3: {
+                  1:    {'disk_size': 20000, 'raid_level': 0},
+                  2:    {'disk_size': 16000, 'raid_level': 0},
+                  },
+              4: {
+                  1:    {'disk_size': 25000, 'raid_level': 0, 'all_eth': True},
+                  2:    {'disk_size': 18000, 'raid_level': 0, 'all_eth': True},
+                  },
+              5: {
+                  1:    {'disk_size': 60000, 'raid_level': 0},
+                  2:    {'disk_size': 30000, 'raid_level': 0},
+                  }
+              }
+          }
+
 param = {
              "chassis_id":      1,
              "cartridge_id":    5,
-             "server_id":       1,
-             "tag_disk_size": "16000",
+             "server_id":       2,
+             "tag_disk_size": "18000",
+             
+             
+             #"tag_disk_group_config_policy_name": "raid0striped",
+             #"tag_raid_level": "raid-0-striped",
+             
+             "tag_disk_group_config_policy_name": "raid1mirrored",
+             "tag_raid_level": "raid-1-mirrored",
+             
              
              "eth_pxe_name_prefix": "eth",
              "eth_pxe_name_index":   0,
@@ -15,9 +50,6 @@ param = {
              "tag_kvm_ip_netmask":  "255.255.255.0",
 
              "tag_boot_policy": "bp-disk-pxe",
-
-             "tag_disk_group_config_policy_name": "raid0striped",
-             "tag_raid_level": "raid-0-striped",
              
              "tag_eth_vlan":    "vlan10",
              "tag_eth_fabric":  "a",
@@ -33,3 +65,124 @@ param = {
              #"tag_local_lun":   "lun132",
              #"tag_volumn_name": "volumn132",
     }
+
+
+
+def create_boot_policy(ucsm_ssh, param):
+    file_text_step = Define.PATH_SNIC_TEXT_UCSM + "boot_policy.txt"   
+    Util.run_text_step(ucsm_ssh, file_text_step, param)
+    
+    
+    
+def create_kvm_console_ip_pool(ucsm_ssh, param):
+    file_text_step = Define.PATH_SNIC_TEXT_UCSM + "kvm_console_ip_pool.txt"   
+    Util.run_text_step(ucsm_ssh, file_text_step, param)
+    
+    
+def create_disk_group_config_policy(ucsm_ssh, param):
+    file_text_step = Define.PATH_SNIC_TEXT_UCSM + "disk_group_config_policy.txt"   
+    Util.run_text_step(ucsm_ssh, file_text_step, param)
+    
+    
+def create_service_profile(ucsm_ssh, param):
+    chassis     = str(param['chassis_id'])
+    cartridge   = str(param['cartridge_id'])
+    server      = str(param['server_id'])
+    
+    chassis_id      = str(chassis).zfill(2)
+    cartridge_id    = str(cartridge).zfill(2)
+    server_id       = str(server).zfill(2)
+    eth_id          = str(param['eth_pxe_name_index']).zfill(2)
+    
+    server_full_id_list = [chassis_id, cartridge_id, server_id]
+    server_full_id  = ''.join(server_full_id_list)
+    
+    eth_full_id_list = [chassis_id, cartridge_id, server_id, eth_id]
+    
+    param['tag_service_profile_name'] = '-'.join(['sp', chassis, cartridge, server])
+    param['tag_local_lun'] = ''.join(['lun', server_full_id])
+    param['tag_volumn_name'] = ''.join(['volumn', server_full_id])
+    param['tag_mac_address'] = ':'.join([param['mac_prefix'], ':'.join(eth_full_id_list)])
+    param["tag_eth_name"] = ''.join([param["eth_pxe_name_prefix"], str(param["eth_pxe_name_index"])])
+    param['tag_uuid'] = ''.join([param['uuid_prefix'], server_full_id])
+    
+    #pprint.pprint(param)
+    
+    file_text_step = Define.PATH_SNIC_TEXT_UCSM + "service_profile.txt"   
+    Util.run_text_step(ucsm_ssh, file_text_step, param)
+    
+    
+def create_eth_if_in_service_profile(ucsm_ssh, param):
+    chassis     = str(param['chassis_id'])
+    cartridge   = str(param['cartridge_id'])
+    server      = str(param['server_id'])
+    
+    chassis_id      = str(chassis).zfill(2)
+    cartridge_id    = str(cartridge).zfill(2)
+    server_id       = str(server).zfill(2)
+    eth_id          = str(param['eth_pxe_name_index']).zfill(2)
+    
+    param['tag_service_profile_name'] = '-'.join(['sp', chassis, cartridge, server])
+    
+    eth_id_number_list = range(2, 8)
+    eth_id_number_list.insert(0, 1880)
+    for eth_id_number in eth_id_number_list: 
+        eth_id = str(eth_id_number).zfill(2)
+        if eth_id_number  == 1880:
+            eth_id = '01'
+        eth_full_id_list = [chassis_id, cartridge_id, server_id, eth_id]
+        vlan_id = str(120 + eth_id_number)
+        param['tag_mac_address'] = ':'.join([param['mac_prefix'], ':'.join(eth_full_id_list)])
+        param["tag_eth_name"] = ''.join([param["eth_pxe_name_prefix"], vlan_id])
+        param["tag_eth_vlan"] = ''.join(["vlan", vlan_id])
+        param['tag_eth_order'] = str(int(eth_id) + 1)
+        if eth_id_number == 1880 or eth_id_number % 2 == 1:
+            param["tag_eth_fabric"] = 'b'
+        else:
+            param["tag_eth_fabric"] = 'a'
+        pprint.pprint(param)
+        file_text_step = Define.PATH_SNIC_TEXT_UCSM + "service_profile_eth_vlan.txt"   
+        Util.run_text_step(ucsm_ssh, file_text_step, param)
+    
+    
+def associate_service_profile(ucsm_ssh, param):
+    chassis     = str(param['chassis_id'])
+    cartridge   = str(param['cartridge_id'])
+    server      = str(param['server_id'])
+    
+    server_full_list = [chassis, cartridge, server]
+    param['tag_service_profile_name'] = '-'.join(['sp', chassis, cartridge, server])
+    param['tag_server_full_id'] = '/'.join(server_full_list)
+    
+    file_text_step = Define.PATH_SNIC_TEXT_UCSM + "service_profile_association.txt"   
+    Util.run_text_step(ucsm_ssh, file_text_step, param)
+    
+    
+def disassociate_service_profile(ucsm_ssh, param):
+    chassis     = str(param['chassis_id'])
+    cartridge   = str(param['cartridge_id'])
+    server      = str(param['server_id'])
+    
+    server_full_list = [chassis, cartridge, server]
+    param['tag_service_profile_name'] = '-'.join(['sp', chassis, cartridge, server])
+    param['tag_server_full_id'] = '/'.join(server_full_list)
+    
+    file_text_step = Define.PATH_SNIC_TEXT_UCSM + "service_profile_dis_association.txt"   
+    Util.run_text_step(ucsm_ssh, file_text_step, param)
+    
+    
+def delete_service_profile(ucsm_ssh, param):
+    chassis     = str(param['chassis_id'])
+    cartridge   = str(param['cartridge_id'])
+    server      = str(param['server_id'])
+    
+    param['tag_service_profile_name'] = '-'.join(['sp', chassis, cartridge, server])
+    file_text_step = Define.PATH_SNIC_TEXT_UCSM + "service_profile_deletion.txt"   
+    Util.run_text_step(ucsm_ssh, file_text_step, param)
+    
+    
+    
+    
+    
+    
+    
