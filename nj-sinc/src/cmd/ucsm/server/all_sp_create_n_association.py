e62e90b5cd3602de49da084b5fe372a6b1b81e91
huan.he <huan.he@gmail.com>
2014-08-27 01:08:27 +0000

    modify vlan creation

--- nj-sinc/src/cmd/ucsm/server/all_sp_create_n_association.py
+++ nj-sinc/src/cmd/ucsm/server/all_sp_create_n_association.py
@@ -25,29 +25,22 @@ if __name__ == '__main__':
         for cartridge_id, cartridge in chassis.iteritems():
             for server_id, server in cartridge.iteritems():
                 
-                lun = server['lun']
-                boot_policy = server['boot_policy']
-                boot_policy = 'disk-pxe-' + boot_policy
-                    
-                eth_cnt     = 1
-                if 'eth_cnt' in server.keys():
-                    eth_cnt = server['eth_cnt']
-                    
                 param['chassis_id']     = chassis_id
                 param['cartridge_id']   = cartridge_id
                 param['server_id']      = server_id
-                param['tag_eth_fabric']   = 'a'
-                param['tag_eth_vlan']   = 'vlan10'
-                param['tag_eth_order']   = '1'
+                
+                boot_policy = server['boot_policy']
+                boot_policy = 'disk-pxe-' + boot_policy
                 param['tag_boot_policy'] = boot_policy
                 
                 #pprint.pprint(param)
                 sp_define.create_service_profile(ucsm_ssh, param)
                 
-                eth_cnt -= 1
+                eth_cnt = server['eth_cnt']
                 if eth_cnt > 0:
                     sp_define.create_eth_if_in_service_profile(ucsm_ssh, param, eth_cnt) 
                     
+                lun = server['lun']
                 sp_define.create_lun_in_service_profile(ucsm_ssh, param, lun)
                 
                 #sp_define.associate_service_profile(ucsm_ssh, param)
