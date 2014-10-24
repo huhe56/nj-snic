a63114e0bbda3f8ab5ff631602d1b7fab290210c
huan.he@gmail.com <huan.he@gmail.com@c3881051-5558-5a05-daaa-2409dd267fac>
2014-10-24 22:32:28 +0000

    reboot FI

--- trunk/nj-sinc/src/cmd/linux/ssh_to_all_hosts.py
+++ trunk/nj-sinc/src/cmd/linux/ssh_to_all_hosts.py
@@ -4,11 +4,17 @@ Created on Aug 26, 2014
 @author: huhe
 '''
 
+from main.define import Define
 from cmd.ucsm.server import sp_define
+from lib.node_head import NodeHead
 from lib.node_compute import NodeCompute
 from lib.util import Util
 
 if __name__ == '__main__':
+    
+    head_node = NodeHead(Define.NODE_HEAD_NAME, Define.NODE_DEFAULT_USERNAME)
+    file_json_step = Define.PATH_SNIC_JSON_LINUX + "clear_ssh_known_host.json"   
+    Util.run_step_list(head_node.get_ssh(), file_json_step)
     
     host_ip_list = sp_define.get_all_host_ip()
     result_dict = {}
